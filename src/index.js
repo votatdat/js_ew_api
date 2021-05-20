e1fd5289ca9f9e2a56bbf64d9377d2b6bdb53b94
Vo Tat Dat <tatdat13@gmail.com>
2021-05-20 22:23:57 +0700

    First express app

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-// index.js
-// This is the main entry point of our application
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+
+dotenv.config();
+
+
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => res.send("Hello World!!!"));
+app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
