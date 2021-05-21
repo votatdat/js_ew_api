ed49ec054d6a8a4d5965505a14039b59197171a0
Vo Tat Dat <tatdat13@gmail.com>
2021-05-21 15:59:35 +0700

    updaed updateNote, deleteNote with user

--- src/resolvers/noteResolver.js
+++ src/resolvers/noteResolver.js
@@ -24,7 +24,9 @@ const noteResolvers = {
   Mutation: {
     newNote: async (parent, { content }, { noteModel, user }) => {
       if (!user) {
-        throw new AuthenticationError('You must sign in!');
+        throw new AuthenticationError(
+          'You must be signed in to create a note'
+        );
       }
       let newNote = {
         content: content,
@@ -32,15 +34,35 @@ const noteResolvers = {
       };
       return await noteModel.create(newNote);
     },
-    deleteNote: async (parent, { id }, { noteModel }) => {
+    deleteNote: async (parent, { id }, { noteModel, user }) => {
+      if (!user) {
+        throw new AuthenticationError(
+          'You must be signed in to delete a note'
+        );
+      }
+      const note = await noteModel.findById(id);
+      if (note && String(note.author) !== user.id) {
+        throw new ForbiddenError(
+          'You don\'t have permissions to delete the note'
+        );
+      }
       try {
-        await noteModel.findOneAndDelete({ _id: id });
+        // if everything checks out, remove the note
+        await note.remove();
         return true;
-      } catch (e) {
+      } catch (err) {
+        // if there's an error along the way, return false
         return false;
       }
     },
-    updateNote: async (parent, { content, id }, { noteModel }) => {
+    updateNote: async (parent, { content, id }, { noteModel, user }) => {
+      if (!user) {
+        throw new AuthenticationError('You must be signed in to update a note');
+      }
+      const note = await noteModel.findById(id);
+      if (note && String(note.author) !== user.id) {
+        throw new ForbiddenError('You don\'t have permissions to update the note');
+      }
       return await noteModel.findOneAndUpdate(
         {
           _id: id,
